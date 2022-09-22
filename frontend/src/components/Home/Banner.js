28f6b0bab075abae8c8556cd60ed55f24dc44e3c
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-23 00:39:20 +0100

    ao_wip: add filter to store items

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,39 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import { connect } from "react-redux";
+import { APPLY_TITLE_FILTER } from "../../constants/actionTypes";
+import { agent } from "superagent";
 
-const Banner = () => {
+const mapStateToProps = (state) => ({
+  ...state.itemList
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  filterItemsByTitle: (payload) => dispatch({type: APPLY_TITLE_FILTER, payload})
+})
+
+
+const Banner = (props) => {
+
+  // console.log("Banner",props)
+
+  const searchItems = (e) => {
+    const {value} = e.target
+
+    const lengthOfInput = value.length
+
+    if(lengthOfInput > 3){
+      props.filterItemsByTitle(agent.Items.byTitle(value))
+     
+    }
+  }
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input type="text" id="search-box" placeholder="What is it that you truly desire?" onChange={searchItems} />
           <span> the cool stuff.</span>
         </div>
       </div>
@@ -15,4 +41,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export default connect(mapStateToProps, mapDispatchToProps)(Banner);
