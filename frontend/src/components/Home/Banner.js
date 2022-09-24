c32e4056fa8de7dcdedf0c0ca0d439cfe106999c
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-09-24 23:47:51 +0100

    feat: show search bar only after the user has clicked get"

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import { connect } from "react-redux";
 import { APPLY_TITLE_FILTER } from "../../constants/actionTypes";
@@ -29,18 +29,41 @@ const Banner = (props) => {
       props.filterItemsByTitle("", agent.Items.all());
     }
   };
+
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const showSearchBar = (e) => {
+    e.preventDefault();
+    setIsSearchOpen(true);
+  };
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            type="text"
-            id="search-box"
-            placeholder="What is it that you truly desire?"
-            onChange={searchItems}
-          />
+          <span id="get-part">
+            A place to{" "}
+            <button
+              style={{
+                outline: "none",
+                background: "none",
+                border: "none",
+                padding: "0",
+              }}
+              className="text-white"
+              onClick={showSearchBar}
+            >
+              get
+            </button>
+          </span>
+          {isSearchOpen && (
+            <input
+              type="text"
+              id="search-box"
+              placeholder="What is it that you truly desire?"
+              onChange={searchItems}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
